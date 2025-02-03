baae15a01ddf8969b0773eead95746f416a50315
Sky Maya <skymaya@gmail.com>
2025-02-03 14:54:16 -0500

    improve admin list display and add search field

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Paths
 
-admin.site.register(Paths)
+
+@admin.register(Paths)
+class PathsAdmin(admin.ModelAdmin):
+    list_display = ['dest_url', 'src_path', 'admin_added']
+    search_fields = ['dest_url', 'src_path']
+
