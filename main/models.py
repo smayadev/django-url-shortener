8a062696f29dc54a272f938e8446f38cef726482
Sky Maya <skymaya@gmail.com>
2025-02-05 03:46:39 -0500

    remove unused code and unused imports

--- main/models.py
+++ main/models.py
@@ -2,12 +2,6 @@ import random
 import string
 from django.db import models
 from django.core.validators import URLValidator
-from django.db.models.signals import post_delete, post_save
-from django.dispatch import receiver
-from django.conf import settings
-import redis
-
-# redis_client = redis.StrictRedis.from_url(settings.CACHES["default"]["LOCATION"], decode_responses=True)
 
 
 class Paths(models.Model):
@@ -37,12 +31,3 @@ class Paths(models.Model):
 
     def __str__(self):
         return self.src_path
-    
-
-# @receiver(post_delete, sender=Paths)
-# def delete_url_cache_on_delete(sender, instance, **kwargs):
-#     """
-#     Remove the cached URL path when it is deleted from the db
-#     """
-#     cache_key = f"url:{instance.src_path}"
-#     redis_client.delete(cache_key)
