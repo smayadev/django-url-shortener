09bc9669920eb05176ddcb9e6851eab018ab91a1
Sky Maya <skymaya@gmail.com>
2025-02-05 03:56:27 -0500

    add resolve api endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from .views import (
     ShortenURLApiView,
+    ResolveURLApiView,
 )
 
 urlpatterns = [
-    path('shorten/', ShortenURLApiView.as_view(), name="shorten_url")
+    path('shorten/', ShortenURLApiView.as_view(), name="shorten_url"),
+    path("resolve/<str:short_code>/", ResolveURLApiView.as_view(), name="resolve_url"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -6,12 +7,22 @@ from main.models import Paths
 from .serializers import PathsSerializer
 
 
+class ResolveURLApiView(APIView):
+    """
+    Resolve a short URL to its original destination
+    """
+
+    def get(self, request, short_code, *args, **kwargs):
+        url_object = get_object_or_404(Paths, short_code=short_code)
+        return Response({"dest_url": url_object.dest_url}, status=status.HTTP_200_OK)
+
+
 class ShortenURLApiView(APIView):
 
     def post(self, request, *args, **kwargs):
-        '''
+        """
         Take a long URL and shorten it
-        '''
+        """
         data = {
             'dest_url': request.data.get('dest_url')
         }
