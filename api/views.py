8296a9ea8953a68693c019a9987f36009e3517f5
Sky Maya <skymaya@gmail.com>
2025-02-05 20:04:18 -0500

    remove unused import

--- api/views.py
+++ api/views.py
@@ -1,7 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework import permissions
 from main.models import Paths
 from .serializers import PathsSerializer
 from .permissions import HasUserAPIKey, HasAdminAPIKey
