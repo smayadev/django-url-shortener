126dc4ffcdb7cba88125ad43a747ddd2d03b4e51
Sky Maya <skymaya@gmail.com>
2025-02-06 02:24:41 -0500

    add celery config and initial task to send clicks

---
+++ main/tasks.py
@@ -0,0 +1,28 @@
+import pika
+import json
+from celery import shared_task
+from django.utils.timezone import now
+
+RABBITMQ_HOST = "rabbitmq"
+
+@shared_task
+def send_click_to_rabbitmq(short_code, ip_address, user_agent, referrer):
+    """
+    Sends click data to RabbitMQ asynchronously
+    """
+    click_data = {
+        "short_code": short_code,
+        "ip_address": ip_address,
+        "user_agent": user_agent,
+        "referrer": referrer,
+        "timestamp": now().isoformat()
+    }
+
+    try:
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST))
+        channel = connection.channel()
+        channel.exchange_declare(exchange="clicks", exchange_type="fanout")
+        channel.basic_publish(exchange="clicks", routing_key="", body=json.dumps(click_data))
+        connection.close()
+    except Exception as e:
+        print(f"Failed to send click data to RabbitMQ: {e}")
