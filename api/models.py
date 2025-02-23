e96899f08047c49a2a3bc83af00a846a2744f8e2
Sky Maya <skymaya@gmail.com>
2025-02-23 00:33:12 -0500

    add is_system field for api keys

--- api/models.py
+++ api/models.py
@@ -4,6 +4,9 @@ from rest_framework_api_key.models import AbstractAPIKey
 class PathsAPIKey(AbstractAPIKey):
     """
     API Keys for accessing the Paths API.
-    The `is_admin` field determines whether the key has full access.
+    The is_admin field determines whether the key has full access.
+    The is_system field determines whether the key has access to 
+    system API endpoints such as captcha.
     """
     is_admin = models.BooleanField(default=False)
+    is_system = models.BooleanField(default=False)
